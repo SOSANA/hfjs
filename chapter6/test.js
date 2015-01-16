c2283119dd172ba25b94fbe8a701fd197b66ac35
sosana <thesosana@gmail.com>
2015-01-16 22:19:26 +0000

    updating chapter 7

---
+++ chapter6/test.js
@@ -0,0 +1,39 @@
+// Playing with attributes!
+/*
+var scoop = document.getElementById("raspberry");
+var altText = scoop.getAttribute("alt");
+if (altText == null) {
+  console.log("oh, I guess there isn't an alt attribute.");
+} else {
+    console.log("I can't see the image in the console,");
+    console.log("but I'm told it looks like " + altText);
+}
+*/
+
+// Sharpen your pencil exercise
+/*
+function addSongs() {
+  var song1 = document.getElementById("song1");
+  var song2 = document.getElementById("song2");
+  var song3 = document.getElementById("song3");
+  
+  song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
+  song2.innerHTML = "Great Objects on fire, by Jerrry JSON Lewis";
+  song3.innerHTML = "I code the Line, by Johnny JavaScript";
+}
+
+window.onload = addSongs;
+*/
+
+// Changing the element object by getElementID and outputing different string
+/* 
+var planet = document.getElementById("greenplanet");
+planet.innerHTML = "Red Alert: hit by phaser fire!";
+console.log(planet.innerHTML);
+*/
+
+//Grabbing element object by getElementId and outputing to console.log
+/*
+var planet = document.getElementById("greenplanet");
+console.log(planet.innerHTML);
+*/
