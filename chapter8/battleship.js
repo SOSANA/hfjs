b9aa0cc62127e19b6a52078fdca5ae114c31f335
sosana <thesosana@gmail.com>
2015-02-11 01:17:43 +0000

    updating chapter 8

---
+++ chapter8/battleship.js
@@ -0,0 +1,20 @@
+
+//Notice we're defining an object and assigning it to the variable view
+var view = {
+  //these methods take a string and message and displays it in the  messsage display area
+  
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  
+  displayHit: function(location) {
+    
+  },
+ 
+  displayMiss: function(location) {
+    
+  },
+ 
+ 
+};
