2b47625f291afcc68a967d2f4ae0315f06d21b58
sosana <thesosana@gmail.com>
2015-02-15 01:23:50 +0000

    updating chapter 8

--- chapter8/battleship.js
+++ chapter8/battleship.js
@@ -1,29 +1,3 @@
-
-// start of the model, holds game state
-var model = {
-  
-  // the size of the grid board for the board
-  boardSize: 7,
-  // the number of ships in the game
-  numShips: 3,
-  // the number of locations in each ship
-  shipLength: 3,
-  // how many ships have been sunk
-  shipsSunk: 0, 
-  // the ship locations and hits
-  ships: [
-		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
-		{ locations: ["10", "11", "12"], hits: ["", "", ""] }
-	],
-  
-  // a method to fire on a ship and figure out if the shot is a hit or miss
-  fire: function() {
-  
-  } 
-}
-// End of model
-
 // start of view
 // Notice we're defining an object and assigning it to the variable view
 var view = {
@@ -50,7 +24,69 @@ var view = {
  
 };
 
+// start of the model, holds game state
+var model = {
+  // the size of the grid board for the board
+  boardSize: 7,
+  // the number of ships in the game
+  numShips: 3,
+  // how many ships have been sunk
+  shipsSunk: 0, 
+  // the number of locations in each ship
+  shipLength: 3,
+  // the ship locations and hits
+  ships: [
+		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+		{ locations: ["10", "11", "12"], hits: ["", "", ""] }
+	],
+  // a method to fire on a ship and figure out if the shot is a hit or miss
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+  //notifies the view when we get a hit or a miss in the model
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;      
+      }
+    }
+    return true; 
+  }
+};
 
+model.fire("53"); // miss
+
+model.fire("06"); // hit
+model.fire("16"); // hit
+model.fire("26"); // hit
+
+model.fire("34"); // hit
+model.fire("24"); // hit
+model.fire("44"); // hit
+
+model.fire("12"); // hit
+model.fire("11"); // hit
+model.fire("10"); // hit
+
+// End of model
 
 
 
