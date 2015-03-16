cb0ce220f76bcd63eb50e9efde73c043d4b419ec
sosana <thesosana@gmail.com>
2015-03-16 18:50:16 +0000

    doing a test on the sort method

--- chapter10/test.js
+++ chapter10/test.js
@@ -1,4 +1,5 @@
-// sharpen your pencil 
+// sharpen your pencil 1
+/*
 function addN(n) {
   var adder = function(x) {
       return n + x;
@@ -9,3 +10,19 @@ function addN(n) {
 var add2  = new addN(2);
 console.log(add2(10));
 console.log(add2(100));
+*/
+
+// sort method that, given a function that knows how to compare two items in the array, 
+// sorts the array for you. Here's an sort method example.
+var numbersArray = [60, 50, 62, 58, 54, 54];
+function compareNumbers(num1, num2) {
+    if (num1 > num2) {
+        return 1;
+    } else if (num1 === num2) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+// sharpen your pencil 2
