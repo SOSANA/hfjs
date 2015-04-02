5d04f079ab786803e895c043adc8db7dd8db3be6
sosana <thesosana@gmail.com>
2015-04-02 18:45:39 +0000

    updating comments for tests

---
+++ chapter13/test.js
@@ -0,0 +1,11 @@
+/*
+ * Extra strength objects
+ * 
+ * JavaScript doesn’t have a classical object-oriented model, where you create objects from classes. 
+ * In fact, JavaScript doesn’t have classes at all . In JavaScript, objects inherit behavior from 
+ * other objects , which we call prototypal inheritance , or inheritance based on prototypes. Note 
+ * This may change in the future: the next version of JavaScript may add classes.  
+ * 
+ * 
+*/
+
