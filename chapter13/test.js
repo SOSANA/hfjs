8da3ff6abbdab4cf3fa7aba21ebbe8e9bdcf2db0
sosana <thesosana@gmail.com>
2015-04-06 18:45:05 +0000

    updating comments for prototype tests

--- chapter13/test.js
+++ chapter13/test.js
@@ -6,6 +6,14 @@
  * other objects , which we call prototypal inheritance , or inheritance based on prototypes. Note 
  * This may change in the future: the next version of JavaScript may add classes.  
  * 
+ * Typically when we talk about an object’s “behavior” we’re referring to the set of methods it supports.
+ * 
+ * Javascript Prototypes
+ * Objects that can inherit properties and behavior from other objects. More 
+ * specifically, JavaScript uses what is known as prototypal inheritance , and the object you’re inheriting 
+ * behavior from is called the prototype . The whole point of this scheme is to inherit and reuse existing 
+ * properties (including methods), while extending those properties in your brand new object. When an object 
+ * inherits from another, it gains access to all its methods and properties.
  * 
 */
 
