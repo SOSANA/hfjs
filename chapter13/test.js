92061d1b9db4676a16a9e99d0e042dada21e569e
sosana <thesosana@gmail.com>
2015-04-07 18:46:36 +0000

    Adding new sit method to the Dog Prototype and updating prototype comments

--- chapter13/test.js
+++ chapter13/test.js
@@ -9,22 +9,61 @@
  * Typically when we talk about an object’s “behavior” we’re referring to the set of methods it supports.
  * 
  * Javascript Prototypes
- * Objects that can inherit properties and behavior from other objects. More 
+ * Objects that can inherit properties and behavior (method's) from other objects. More 
  * specifically, JavaScript uses what is known as prototypal inheritance , and the object you’re inheriting 
- * behavior from is called the prototype . The whole point of this scheme is to inherit and reuse existing 
+ * behavior from is called the prototype. The whole point of this scheme is to inherit and reuse existing 
  * properties (including methods), while extending those properties in your brand new object. When an object 
- * inherits from another, it gains access to all its methods and properties.
+ * inherits from another, it gains access to all its methods and properties. Adding new methods or properties 
+ * to a prototype allows all objects to inherit the new methods or properties. All object instances inherit
+ * from the Prototype.
  * 
 */
 
+// Teaching a dog a new trick (dynamic prototypes)
+/*
+ * We’re glad to see Barnaby can now sit. But it turns out that now all our dogs can sit, because once you 
+ * add a method to a prototype, any objects that inherit from that prototype can make use of that method:
+ * This works for properties too, of course.
+*/
+function Dog(name, breed, weight) {
+	this.name = name;
+	this.breed = breed;
+	this.weight = weight;
+}
 
+Dog.prototype.species = "Canine";
+
+Dog.prototype.bark = function() { 
+	if (this.weight > 25) {
+		console.log(this.name + " says Woof!"); 
+	} else {
+		console.log(this.name + " says Yip!"); 
+	}
+};
+
+Dog.prototype.run = function() {
+	console.log("Run!"); 
+};
+Dog.prototype.wag = function() {
+	console.log("Wag!"); 
+};
+
+var barnaby = new Dog("Barnaby", "Basset Hound", 55);
+Dog.prototype.sit = function() {
+	console.log(this.name + " is now sitting");
+}
+barnaby.sit();
+
+/*
 // Robots Exercise
+// Robot Object Constructor
 function Robot(name, year, owner) {
 	this.name = name;
 	this.year = year;
 	this.owner = owner;
 }
 
+// Robot Prototype
 Robot.prototype.maker = "ObjectsRus";
 
 Robot.prototype.speak = function speak() {
@@ -39,6 +78,7 @@ Robot.prototype.blinkLights = function blinkLights() {
     alert("Blink blink!");
 };
 
+// Creating new Robots from our Robot Constructor that inherits from Robot Prototype
 var robby = new Robot("Robby", 1956, "Dr.Morbius"); 
 var rosie =  new Robot("Rosie", 1962, "George Jetson");
 
@@ -49,7 +89,7 @@ robby.makeCoffee = function makeCoffee() {
     
 rosie.cleanHouse = function cleanHouse() {
     alert("Cleaning! Spic and Span soon...");
-}
+};
     
 console.log(robby.name + " was made by " + robby.maker + 
             " in " + robby.year + " and is owned by " + robby.owner);
@@ -59,8 +99,17 @@ robby.blinkLights();
 console.log(rosie.name + " was made by " + rosie.maker + 
             " in " + rosie.year + " and is owned by " + rosie.owner);
 rosie.cleanHouse();
+*/
 
 
+/*
+ * When we are calling the bark method in the prototype, you might think that "this" is now set to the prototype 
+ * object. Well, that’s not how it works. When you call an object’s method, "this" is set to the object whose method 
+ * was called. If the method is not found in that object, and is found in the prototype, that doesn’t change the 
+ * value of "this". "this" always refers to the original object—that is, the object whose method was called—even if 
+ * the method is in the prototype. So, if we find the bark method in the prototype, then we call the method, with 
+ * this set to the original dog object, giving us the result we want, like “Fluffy says Woof!”.
+*/
 /*
 // Test Drive the prototype with some dogs & add custom bark method
 // Dog Object Constructor
