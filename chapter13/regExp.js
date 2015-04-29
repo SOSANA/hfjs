4db94b2123b9f57cca9c0d4bc59b9fcbfef24690
sosana <thesosana@gmail.com>
2015-04-29 18:43:46 +0000

    adding in a regexp for matching phone numbers

--- chapter13/regExp.js
+++ chapter13/regExp.js
@@ -1,3 +1,15 @@
 /**
-* 
+ * The key to understanding regular expressions is learning how to read the search patterns. 
+ * These search patterns are the most complex part of regular expressions, so we’ll work 
+ * through the two examples here, and you’ll have to explore the rest on your own.
+ * Refer to appendix p.612
 */
+
+var phoneNumber = new RegExp(/^\d{3}-?\d{4}$/);
+var amyHome = "555-1212";
+var result = amyHome.match(phoneNumber);
+console.log(result);
+
+var invalid = "5556-1212";
+var result2 = invalid.match(phoneNumber);
+console.log(result2);
